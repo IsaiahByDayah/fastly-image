09bb8d779d113b0fb0178a08c4767d18e5577061
Isaiah Smith <jordankid93@gmail.com>
2019-08-06 15:40:47 -0700

    chore: write tests and add comments

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,391 @@
-test('Sanity', () => {
-  expect('Hello').toBe('Hello')
+import { fastlyImageUrl } from '../index'
+
+const IMAGE_URL = 'https://www.example.com/image.jpg'
+
+test('Sanity Cgeck', () => {
+  expect(true).toBe(true)
+})
+
+describe('Width', () => {
+  test('pixels', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { width: 200 })).toEqual(`${IMAGE_URL}?width=200`)
+  })
+
+  test('percentage', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { width: '200p' })).toEqual(`${IMAGE_URL}?width=200p`)
+  })
+})
+
+describe('Height', () => {
+  test('pixels', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { height: 200 })).toEqual(`${IMAGE_URL}?height=200`)
+  })
+
+  test('percentage', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { height: '200p' })).toEqual(`${IMAGE_URL}?height=200p`)
+  })
+})
+
+describe('Device Pixel Ratio', () => {
+  test('integer', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { dpr: 2 })).toEqual(`${IMAGE_URL}?dpr=2`)
+  })
+
+  test('float', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { devicePixelRatio: 1.5 })).toEqual(`${IMAGE_URL}?dpr=1.5`)
+  })
+})
+
+describe('Fit', () => {
+  test('bounds', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { fit: 'bounds' })).toEqual(`${IMAGE_URL}?fit=bounds`)
+  })
+
+  test('cover', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { fit: 'cover' })).toEqual(`${IMAGE_URL}?fit=cover`)
+  })
+
+  test('crop', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { fit: 'crop' })).toEqual(`${IMAGE_URL}?fit=crop`)
+  })
+})
+
+describe('Crop', () => {
+  test('raw', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { crop: 'foo' })).toEqual(`${IMAGE_URL}?crop=foo`)
+  })
+
+  test('size', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { crop: { type: 'Size', height: 900, width: 1600 } })).toEqual(
+      `${IMAGE_URL}?crop=1600,900`
+    )
+  })
+
+  test('coordinates', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { crop: { type: 'Coord', height: 900, width: 1600, x: 100, y: '50p' } })).toEqual(
+      `${IMAGE_URL}?crop=1600,900,x100,y50p`
+    )
+  })
+
+  test('offset', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, { crop: { type: 'Offset', height: 900, width: 1600, xOffset: '100p', yOffset: '50p' } })
+    ).toEqual(`${IMAGE_URL}?crop=1600,900,offset-x100p,offset-y50p`)
+  })
+
+  test('aspect ratio', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { crop: { type: 'AspectRatio', height: 9, width: 16 } })).toEqual(
+      `${IMAGE_URL}?crop=16:9`
+    )
+  })
+
+  test('aspect ratio w/ offset', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, {
+        crop: { type: 'AspectRatioOffset', height: 9, width: 16, xOffset: '100p', yOffset: '50p' },
+      })
+    ).toEqual(`${IMAGE_URL}?crop=16:9,offset-x100p,offset-y50p`)
+  })
+
+  test('smart', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, {
+        crop: { type: 'Smart', height: 9, width: 16 },
+      })
+    ).toEqual(`${IMAGE_URL}?crop=16:9,smart`)
+  })
+})
+
+describe('Trim', () => {
+  test('raw', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { trim: 'foo' })).toEqual(`${IMAGE_URL}?trim=foo`)
+  })
+
+  test('values', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { trim: { top: 100, right: '50p', bottom: 200, left: '25p' } })).toEqual(
+      `${IMAGE_URL}?trim=100,50p,200,25p`
+    )
+  })
+})
+
+describe('Padding', () => {
+  test('raw', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { pad: 'foo' })).toEqual(`${IMAGE_URL}?pad=foo`)
+  })
+
+  test('values', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { padding: { top: 100, right: '50p', bottom: 200, left: '25p' } })).toEqual(
+      `${IMAGE_URL}?pad=100,50p,200,25p`
+    )
+  })
+})
+
+describe('Canvas', () => {
+  test('raw', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { canvas: 'foo' })).toEqual(`${IMAGE_URL}?canvas=foo`)
+  })
+
+  test('size', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { canvas: { type: 'Size', height: 900, width: 1600 } })).toEqual(
+      `${IMAGE_URL}?canvas=1600,900`
+    )
+  })
+
+  test('coordinates', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, { canvas: { type: 'Coord', height: 900, width: 1600, x: 100, y: '50p' } })
+    ).toEqual(`${IMAGE_URL}?canvas=1600,900,x100,y50p`)
+  })
+
+  test('offset', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, {
+        canvas: { type: 'Offset', height: 900, width: 1600, xOffset: '100p', yOffset: '50p' },
+      })
+    ).toEqual(`${IMAGE_URL}?canvas=1600,900,offset-x100p,offset-y50p`)
+  })
+
+  test('aspect ratio', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { canvas: { type: 'AspectRatio', height: 9, width: 16 } })).toEqual(
+      `${IMAGE_URL}?canvas=16:9`
+    )
+  })
+
+  test('aspect ratio w/ offset', () => {
+    expect(
+      fastlyImageUrl(IMAGE_URL, {
+        canvas: { type: 'AspectRatioOffset', height: 9, width: 16, xOffset: '100p', yOffset: '50p' },
+      })
+    ).toEqual(`${IMAGE_URL}?canvas=16:9,offset-x100p,offset-y50p`)
+  })
+})
+
+describe('Background Color', () => {
+  test('hex', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { bgColor: 'fff' })).toEqual(`${IMAGE_URL}?bg-color=fff`)
+  })
+
+  test('rgb', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { bgColor: { r: 255, g: 255, b: 255 } })).toEqual(
+      `${IMAGE_URL}?bg-color=255,255,255`
+    )
+  })
+
+  test('rgba', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { backgroundColor: { r: 255, g: 255, b: 255, a: 0.5 } })).toEqual(
+      `${IMAGE_URL}?bg-color=255,255,255,0.5`
+    )
+  })
+})
+
+describe('Orientation', () => {
+  test('default', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'Default' })).toEqual(`${IMAGE_URL}?orient=1`)
+  })
+
+  test('rotate right', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'RotateRight' })).toEqual(`${IMAGE_URL}?orient=r`)
+  })
+
+  test('rotate left', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'RotateLeft' })).toEqual(`${IMAGE_URL}?orient=l`)
+  })
+
+  test('flip horizontally', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'FlipHorizontal' })).toEqual(`${IMAGE_URL}?orient=h`)
+  })
+
+  test('flip vertically', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'FlipVertical' })).toEqual(`${IMAGE_URL}?orient=v`)
+  })
+
+  test('upside-down', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'Upsidedown' })).toEqual(`${IMAGE_URL}?orient=hv`)
+  })
+
+  test('mirror left', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'MirrorLeft' })).toEqual(`${IMAGE_URL}?orient=5`)
+  })
+
+  test('mirror right', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { orient: 'MirrorRight' })).toEqual(`${IMAGE_URL}?orient=7`)
+  })
+})
+
+describe('Brightness', () => {
+  test('integer', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { bright: -50 })).toEqual(`${IMAGE_URL}?brightness=-50`)
+  })
+
+  test('float', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { brightness: 25.75 })).toEqual(`${IMAGE_URL}?brightness=25.75`)
+  })
+})
+
+describe('Contrast', () => {
+  test('integer', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { con: -50 })).toEqual(`${IMAGE_URL}?contrast=-50`)
+  })
+
+  test('float', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { contrast: 25.75 })).toEqual(`${IMAGE_URL}?contrast=25.75`)
+  })
+})
+
+describe('Saturation', () => {
+  test('integer', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { sat: -50 })).toEqual(`${IMAGE_URL}?saturation=-50`)
+  })
+
+  test('float', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { saturation: 25.75 })).toEqual(`${IMAGE_URL}?saturation=25.75`)
+  })
+})
+
+describe('Sharpen', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { sharpen: { amount: 5, radius: 500, threshold: 156 } })).toEqual(
+      `${IMAGE_URL}?sharpen=a5,r500,t156`
+    )
+  })
+})
+
+describe('Blur', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { blur: 500 })).toEqual(`${IMAGE_URL}?blur=500`)
+  })
+})
+
+describe('Format', () => {
+  test('gif', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'gif' })).toEqual(`${IMAGE_URL}?format=gif`)
+  })
+
+  test('jpg', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'jpg' })).toEqual(`${IMAGE_URL}?format=jpg`)
+  })
+
+  test('pjpg', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'pjpg' })).toEqual(`${IMAGE_URL}?format=pjpg`)
+  })
+
+  test('png', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'png' })).toEqual(`${IMAGE_URL}?format=png`)
+  })
+
+  test('png8', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'png8' })).toEqual(`${IMAGE_URL}?format=png8`)
+  })
+
+  test('webp', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'webp' })).toEqual(`${IMAGE_URL}?format=webp`)
+  })
+
+  test('webpll', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'webpll' })).toEqual(`${IMAGE_URL}?format=webpll`)
+  })
+
+  test('webply', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { format: 'webply' })).toEqual(`${IMAGE_URL}?format=webply`)
+  })
+})
+
+describe('Frame', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { frame: true })).toEqual(`${IMAGE_URL}?frame=1`)
+  })
+})
+
+describe('Quality', () => {
+  test('default', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { quality: 50 })).toEqual(`${IMAGE_URL}?quality=50`)
+  })
+
+  test('w/ webp value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { quality: [50, 75] })).toEqual(`${IMAGE_URL}?quality=50,75`)
+  })
+
+  test('not enough numbers supplied', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { quality: [] })).toEqual(`${IMAGE_URL}`)
+  })
+
+  test('too many numbers supplied', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { quality: [1, 2, 3] })).toEqual(`${IMAGE_URL}`)
+  })
+})
+
+describe('Auto', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { auto: true })).toEqual(`${IMAGE_URL}?auto=webp`)
+  })
+})
+
+describe('Upscaling', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { upscaling: true })).toEqual(`${IMAGE_URL}?enable=upscale`)
+  })
+})
+
+describe('Resize Filter', () => {
+  test('nearest', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { resFil: 'nearest' })).toEqual(`${IMAGE_URL}?resize-filter=nearest`)
+  })
+
+  test('bilinear', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { resizeFilter: 'bilinear' })).toEqual(`${IMAGE_URL}?resize-filter=bilinear`)
+  })
+
+  test('bicubic', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { resizeFilter: 'bicubic' })).toEqual(`${IMAGE_URL}?resize-filter=bicubic`)
+  })
+
+  test('lanczos', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { resizeFilter: 'lanczos' })).toEqual(`${IMAGE_URL}?resize-filter=lanczos`)
+  })
+
+  test('lanczos2', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { resizeFilter: 'lanczos2' })).toEqual(`${IMAGE_URL}?resize-filter=lanczos2`)
+  })
+})
+
+describe('Upscaling', () => {
+  test('value', () => {
+    expect(fastlyImageUrl(IMAGE_URL, { upscaling: true })).toEqual(`${IMAGE_URL}?enable=upscale`)
+  })
+})
+
+describe('Number of Fastly Params', () => {
+  describe('Zero', () => {
+    test('Incoming URL w/o params', () => {
+      expect(fastlyImageUrl(IMAGE_URL, {})).toEqual(IMAGE_URL)
+    })
+
+    test('Incoming URL w/ params', () => {
+      expect(fastlyImageUrl(`${IMAGE_URL}?foo=bar`, {})).toEqual(`${IMAGE_URL}?foo=bar`)
+    })
+  })
+
+  describe('One', () => {
+    test('Incoming URL w/o params', () => {
+      expect(fastlyImageUrl(IMAGE_URL, { height: 100 })).toEqual(`${IMAGE_URL}?height=100`)
+    })
+
+    test('Incoming URL w/ params', () => {
+      expect(fastlyImageUrl(`${IMAGE_URL}?foo=bar`, { height: 100 })).toEqual(`${IMAGE_URL}?foo=bar&height=100`)
+    })
+  })
+
+  describe('Multiple', () => {
+    test('Incoming URL w/o params', () => {
+      expect(fastlyImageUrl(IMAGE_URL, { height: 100, width: 200, auto: true, blur: 500 })).toEqual(
+        `${IMAGE_URL}?auto=webp&blur=500&height=100&width=200`
+      )
+    })
+
+    test('Incoming URL w/ params', () => {
+      expect(fastlyImageUrl(`${IMAGE_URL}?foo=bar`, { height: 100, width: 200, auto: true, blur: 500 })).toEqual(
+        `${IMAGE_URL}?auto=webp&blur=500&foo=bar&height=100&width=200`
+      )
+    })
+  })
 })
--- src/fastlyImageUrl.ts
+++ src/fastlyImageUrl.ts
@@ -42,7 +42,7 @@ const cropToString = (crop: Crop) => {
       case 'AspectRatioOffset':
         return `${crop.width}:${crop.height},offset-x${crop.xOffset},offset-y${crop.yOffset}`
       case 'Smart':
-        return `${crop.width},${crop.height},smart`
+        return `${crop.width}:${crop.height},smart`
     }
   }
 }
@@ -53,8 +53,11 @@ const cropToString = (crop: Crop) => {
  * @member height The height of the image]
  */
 interface SizeValues<T = 'Size'> {
+  /** The type of value */
   type: T
+  /** The width of the image */
   width: PixelOrPercentage
+  /** The width of the image */
   height: PixelOrPercentage
 }
 
@@ -66,7 +69,9 @@ interface SizeValues<T = 'Size'> {
  * @member y The y coordinate sub-region of the source image
  */
 interface CoordValues extends SizeValues<'Coord'> {
+  /** The x coordinate sub-region of the source image */
   x: PixelOrPercentage
+  /** The y coordinate sub-region of the source image */
   y: PixelOrPercentage
 }
 
@@ -78,7 +83,9 @@ interface CoordValues extends SizeValues<'Coord'> {
  * @member yOffset The vertical offset of the source image
  */
 interface OffsetValues<T = 'Offset'> extends SizeValues<T> {
+  /** The horizontal offset of the source image */
   xOffset: Percentage
+  /** The vertical offset of the source image */
   yOffset: Percentage
 }
 
@@ -108,15 +115,21 @@ type SmartValues = SizeValues<'Smart'>
  * Describes values for the edges of an image
  */
 interface EdgeValues {
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   top: PixelOrPercentage
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   right: PixelOrPercentage
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   bottom: PixelOrPercentage
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   left: PixelOrPercentage
 }
 const edgeValuesToString = ({ top, right, bottom, left }: EdgeValues): string => `${top},${right},${bottom},${left}`
 
 /**
  * An edge values object of how much to trim from each side or a raw value string
+ *
+ * As a string, values can be specified using CSS style shorthand values
  */
 type Trim = EdgeValues | string
 const trimToString = (trim: Trim): string => {
@@ -129,6 +142,8 @@ const trimToString = (trim: Trim): string => {
 
 /**
  * An edge values object of how much to pad to each side or a raw value string
+ *
+ * As a string, values can be specified using CSS style shorthand values
  *
  * Ignored if `canvas` is also supplied
  */
@@ -165,7 +180,7 @@ const canvasToString = (canvas: Canvas) => {
 }
 
 /**
- * A hex value string (without '#')
+ * A hex value string (without `#`)
  */
 type Hex = string
 
@@ -176,12 +191,19 @@ type Hex = string
  * @member a Alpha color value between `0.0` and `1.0`
  */
 interface RGBA {
+  /** Red color value between `0` and `255` */
   r: number
+  /** Green color value between `0` and `255` */
   g: number
+  /** Blue color value between `0` and `255` */
   b: number
+  /** Alpha color value between `0.0` and `1.0` */
   a?: number
 }
 
+/**
+ * A hex value string (without `#`) OR an object describing the color via rgba
+ */
 type Color = Hex | RGBA
 const colorToString = (color: Color) =>
   typeof color === 'string' ? color : `${color.r},${color.g},${color.b}${color.a !== undefined ? `,${color.a}` : ''}`
@@ -189,16 +211,18 @@ const colorToString = (color: Color) =>
 /**
  * Orientation style to apply to image
  */
-enum Orientation {
-  Default = '1',
-  RotateRight = 'r',
-  RotateLeft = 'l',
-  FlipHorizontal = 'h',
-  FlipVertical = 'v',
-  Upsidedown = 'hv',
-  MirrorLeft = '5',
-  MirrorRight = '7',
+type Orientation = keyof typeof Orientations
+const Orientations = {
+  Default: '1',
+  RotateRight: 'r',
+  RotateLeft: 'l',
+  FlipHorizontal: 'h',
+  FlipVertical: 'v',
+  Upsidedown: 'hv',
+  MirrorLeft: '5',
+  MirrorRight: '7',
 }
+const orientationToString = (orientation: Orientation) => Orientations[orientation]
 
 /**
  * Value between `-100` and `100`
@@ -235,8 +259,11 @@ type Saturation = number
  * @member threshold value between `0` and `255`
  */
 interface Sharpen {
+  /** value between `0` and `10` */
   amount: number
+  /** value between `1` and `1000` */
   radius: number
+  /** value between `0` and `255` */
   threshold: number
 }
 const sharpenToString = ({ amount, radius, threshold }: Sharpen) => `a${amount},r${radius},t${threshold}`
@@ -268,7 +295,7 @@ const frameToString = (frame: Frame) => (frame ? '1' : undefined)
 /**
  * Number between `1` and `100`
  *
- * Output image quality for lossy file formats
+ * Sets the output image's quality for lossy file formats
  *
  * If the `auto` is enabled through params or in the service settings, an array of 2 numbers can be supplied with the second value used as the quality value if the requestor sends the `accept: image/webp` request header
  */
@@ -282,15 +309,15 @@ const qualityToString = (quality: Quality) =>
  * Overrides the `format` parameter in browsers that support WebP
  */
 type Auto = boolean
-const autoToString = (auto: Auto) => (auto ? 'auto=webp' : undefined)
+const autoToString = (auto: Auto) => (auto ? 'webp' : undefined)
 
 /**
  * Enables image upscaling
  *
- * Must be used with the `width`, `height` or `dpr/devicePixelRatio` parameters to see the effects in the output image
+ * Must be used with the `width`, `height` or `devicePixelRatio` parameters to see the effects in the output image
  */
 type Upscaling = boolean
-const upscalingToString = (upscaling: Upscaling) => (upscaling ? 'enable=upscale' : undefined)
+const upscalingToString = (upscaling: Upscaling) => (upscaling ? 'upscale' : undefined)
 
 /**
  * Control the resizing filter used to generate a new image with a higher or lower number of pixels
@@ -306,39 +333,177 @@ const upscalingToString = (upscaling: Upscaling) => (upscaling ? 'enable=upscale
 type ResizeFilter = 'nearest' | 'bilinear' | 'bicubic' | 'lanczos' | 'lanczos2'
 
 export interface FastlyImageParams {
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   width?: PixelOrPercentage
+  /** Either a whole number for pixels or a string of format `${whole number}p` for percentage */
   height?: PixelOrPercentage
-  dpr?: DevicePixelRation // Alias for `devicePixelRatio`
+  /** Whole number between `1` and `10000` or float between `0.0` and `9999.999` */
+  dpr?: DevicePixelRation
+  /**
+   * Whole number between `1` and `10000` or float between `0.0` and `9999.999`
+   * @alias dpr
+   */
   devicePixelRatio?: DevicePixelRation
   fit?: 'bounds' | 'cover' | 'crop'
+  /** A croping scheme object to apply to the image or raw value string */
   crop?: Crop
+  /**
+   * An edge values object of how much to trim from each side or a raw value string
+   *
+   * As a string, values can be specified using CSS style shorthand values
+   */
   trim?: Trim
-  pad?: Padding // Alias for `padding`
+  /**
+   * An edge values object of how much to pad to each side or a raw value string
+   *
+   * As a string, values can be specified using CSS style shorthand values
+   *
+   * Ignored if `canvas` is also supplied
+   */
+  pad?: Padding
+  /**
+   * An edge values object of how much to pad to each side or a raw value string
+   *
+   * As a string, values can be specified using CSS style shorthand values
+   *
+   * Ignored if `canvas` is also supplied
+   * @alias pad
+   */
   padding?: Padding
+  /** Object describing the size of the canvas around an image or a raw value string */
   canvas?: Canvas
-  bgColor?: Color // Alias for `backgroundColor`
+  /** A hex value string (without `#`) OR an object describing the color via rgba */
+  bgColor?: Color
+  /**
+   * A hex value string (without `#`) OR an object describing the color via rgba
+   * @alias bgColor
+   */
   backgroundColor?: Color
-  orient?: Orientation // Alias for `orientation`
+  /** Orientation style to apply to image */
+  orient?: Orientation
+  /**
+   * Orientation style to apply to image
+   * @alias orient
+   */
   orientation?: Orientation
-  bright?: Brightness // Alias for `brightness`
+  /**
+   * Value between `-100` and `100`
+   *
+   * Increases or decreases the amount of perceived light an image radiates or reflects
+   *
+   * `-100` will be fully black. `100` will be fully white. Default is `0`
+   */
+  bright?: Brightness
+  /**
+   * Value between `-100` and `100`
+   *
+   * Increases or decreases the amount of perceived light an image radiates or reflects
+   *
+   * `-100` will be fully black. `100` will be fully white. Default is `0`
+   * @alias bright
+   */
   brightness?: Brightness
-  con?: Contrast // Alias for `constrast`
+  /**
+   * Value between `-100` and `100`
+   *
+   * Increases or decreases the difference between the darkest and lightest tones in an image
+   *
+   * `-100` will be fully black. `100` will be fully white. Default is `0`
+   */
+  con?: Contrast
+  /**
+   * Value between `-100` and `100`
+   *
+   * Increases or decreases the difference between the darkest and lightest tones in an image
+   *
+   * `-100` will be fully black. `100` will be fully white. Default is `0`
+   * @alias con
+   */
   contrast?: Contrast
-  sat?: Saturation // Alias for `saturation`
+  /**
+   * Value between `-100` and `100`
+   *
+   * Increases or decreases the intensity of the colors in an image
+   *
+   * `-100` will be fully black. `100` will be fully white. Default is `0`
+   */
+  sat?: Saturation
+  /**
+   * Value between `-100` and `100`
+   * @alias sat
+   */
   saturation?: Saturation
+  /** Increases the definition of the edges of objects in an image */
   sharpen?: Sharpen
+  /**
+   * Value between `1` and `1000`
+   *
+   * Decreases the definition and focus of an image
+   */
   blur?: Blur
+  /**
+   * Enables the source image to be converted (a.k.a., "transcoded") from one encoded format to another. This is very useful when the source image has been saved in a sub-optimal file format that hinders performance
+   *
+   * The source image can be any of the following image formats: `JPEG`, `PNG`, `GIF`, `WEBP`
+   */
   format?: Format
+  /**
+   * Extract the first frame from an animated image sequence
+   *
+   * Only supported when the source image is an Animated GIF
+   *
+   * The first frame is currently the only frame able to be returned
+   */
   frame?: Frame
+  /**
+   * Number between `1` and `100`
+   *
+   * Sets the output image's quality for lossy file formats
+   *
+   * If the `auto` is enabled through params or in the service settings, an array of 2 numbers can be supplied with the second value used as the quality value if the requestor sends the `accept: image/webp` request header
+   */
   quality?: Quality
+  /**
+   * Turns on functionality that automates certain optimization features
+   *
+   * Overrides the `format` parameter in browsers that support WebP
+   */
   auto?: Auto
+  /**
+   * Enables image upscaling
+   *
+   * Must be used with the `width`, `height` or `devicePixelRatio` parameters to see the effects in the output image
+   */
   upscaling?: Upscaling
-  resFil?: ResizeFilter // Alias for `resizeFilter`
+  /**
+   * Control the resizing filter used to generate a new image with a higher or lower number of pixels
+   *
+   * When making an image smaller, use `bicubic`, which has a natural sharpening effect
+   *
+   * When making an image larger, use `bilinear`, which has a natural smoothing effect
+   *
+   * When resizing pixel art, use `nearest`, which has a natural pixelation effect
+   *
+   * When quality is the main concern, use `lanczos`, which typically renders the best results
+   */
+  resFil?: ResizeFilter
+  /**
+   * Control the resizing filter used to generate a new image with a higher or lower number of pixels
+   *
+   * When making an image smaller, use `bicubic`, which has a natural sharpening effect
+   *
+   * When making an image larger, use `bilinear`, which has a natural smoothing effect
+   *
+   * When resizing pixel art, use `nearest`, which has a natural pixelation effect
+   *
+   * When quality is the main concern, use `lanczos`, which typically renders the best results
+   * @alias resFil
+   */
   resizeFilter?: ResizeFilter
 }
 
 const fastlyImageUrl = (imageUrl: string, params: FastlyImageParams): string => {
-  const { url, query } = queryString.parseUrl(imageUrl)
+  const { url, query } = queryString.parseUrl(imageUrl, { decode: false })
 
   // Destructure
   const {
@@ -367,7 +532,6 @@ const fastlyImageUrl = (imageUrl: string, params: FastlyImageParams): string => 
 
   let { dpr, pad, bgColor, orient, brightness, contrast, saturation, resizeFilter } = params
 
-  // alias handling
   dpr = dpr || devicePixelRatio
   pad = pad || padding
   bgColor = bgColor || backgroundColor
@@ -377,35 +541,36 @@ const fastlyImageUrl = (imageUrl: string, params: FastlyImageParams): string => 
   saturation = sat || saturation
   resizeFilter = resFil || resizeFilter
 
-  const input = {
-    dpr: (dpr !== undefined && dpr) || undefined,
-    pad: (pad && paddingToString(pad)) || undefined,
-    'bg-color': (bgColor && colorToString(bgColor)) || undefined,
-    orient,
-    brightness,
-    contrast,
-    saturation,
-    'resize-filter': resizeFilter,
-    crop: (crop && cropToString(crop)) || undefined,
-    width,
-    height,
-    fit,
-    trim: (trim && trimToString(trim)) || undefined,
-    canvas: (canvas && canvasToString(canvas)) || undefined,
-    sharpen: (sharpen && sharpenToString(sharpen)) || undefined,
-    blur,
-    format,
-    frame: (frame !== undefined && frameToString(frame)) || undefined,
-    quality: (quality !== undefined && qualityToString(quality)) || undefined,
-    auto: (auto !== undefined && autoToString(auto)) || undefined,
-    upscaling: (upscaling !== undefined && upscalingToString(upscaling)) || undefined,
-  }
+  const input = compact(
+    {
+      dpr: (dpr !== undefined && dpr) || undefined,
+      pad: (pad && paddingToString(pad)) || undefined,
+      'bg-color': (bgColor && colorToString(bgColor)) || undefined,
+      orient: (orient && orientationToString(orient)) || undefined,
+      brightness,
+      contrast,
+      saturation,
+      'resize-filter': resizeFilter,
+      crop: (crop && cropToString(crop)) || undefined,
+      width,
+      height,
+      fit,
+      trim: (trim && trimToString(trim)) || undefined,
+      canvas: (canvas && canvasToString(canvas)) || undefined,
+      sharpen: (sharpen && sharpenToString(sharpen)) || undefined,
+      blur,
+      format,
+      frame: (frame !== undefined && frameToString(frame)) || undefined,
+      quality: (quality !== undefined && qualityToString(quality)) || undefined,
+      auto: (auto !== undefined && autoToString(auto)) || undefined,
+      enable: (upscaling !== undefined && upscalingToString(upscaling)) || undefined,
+    },
+    { false: false }
+  )
 
-  const compactedInput = compact(input, { false: false })
+  const finalQuery = { ...query, ...input }
 
-  const finalQuery = { ...query, ...compactedInput }
-
-  return `${url}${isEmpty(finalQuery) ? '' : `?${queryString.stringify(finalQuery)}`}`
+  return `${url}${isEmpty(finalQuery) ? '' : `?${queryString.stringify(finalQuery, { encode: false })}`}`
 }
 
 export default fastlyImageUrl
